e645891caae4f514e022135f5c6780de8730c8a9
Mateus Goulart <mat_goulart3003@yahoo.com.br>
2023-04-13 21:33:42 -0300

    primeiro commit prova

---
+++ mateus_goulart/src/main/java/mateus_goulart/prova1/MateusGoulartApplication.java
@@ -0,0 +1,13 @@
+package mateus_goulart.prova1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateusGoulartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MateusGoulartApplication.class, args);
+	}
+
+}
---
+++ mateus_goulart/src/test/java/mateus_goulart/prova1/MateusGoulartApplicationTests.java
@@ -0,0 +1,13 @@
+package mateus_goulart.prova1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MateusGoulartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
